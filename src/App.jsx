dca8cbc15ee7877380fa52a987148ad049714d21
snehakashyap09 <snehakashyap0928@gmail.com>
2025-06-28 06:08:29 +0530

    skills

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
-import Skills from './components/skills'
+import Skills from './components/Skills.jsx'
 
 function App() {
 
