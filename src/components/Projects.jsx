dae2c8de70a16c06264ee188dfaf6e14637fd9f9
snehakashyap09 <snehakashyap0928@gmail.com>
2025-06-28 14:01:28 +0530

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -33,10 +33,10 @@ const Projects = () => {
       <div className='max-w-5xl mx-auto grid  grid-cols-1 md:grid-cols-2 gap-16'>
       {projects.map((projects,index)=>(
         <div key = {index} className='bg-gray-800 p-6 rounded-lg hover:shadow-lg transform transition-transform duration-300 hover:scale-105'>
-         <img src={projects.image} alt={projects.title} className='rounded-lg mb-4 w-full h-48 object-cover' />
+         <img src={projects.image} alt={projects.title} className='rounded-lg mb-4 w-full h-48 object-contain' />
         <h3 className='text-2xl font-bold text-transparent mb-2 bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>{projects.title}</h3>
         
-        <div className='flex flex-wrap mt-4 gap-2'>
+        <div className='flex flex-wrap  mt-4 gap-2'>
          {projects.tech.map((tech,i)=>(
           <span key={i} className='bg-black border  text-white-300 px-2 sm:px-3 py-1 rounded-full text-xs sm:text-sm md:text-lg'>
             {tech}
@@ -48,9 +48,9 @@ const Projects = () => {
         <p className='text-sm sm:text-base text-gray-300 mb-4 text-justify'>{projects.description}</p>
        
 
-       <div className='flex flex-wrap mt-auto  gap-4'>
+       <div className='flex flex-wrap text-end mt-auto  gap-4'>
         <a href={projects.github} target='blank' title='github'>
-          <FaGithub className='text-green-500 text-2xl hover:text-gray-400 transition ' />
+          <FaGithub className='text-green-500 text-2xl hover:text-gray-400 transition' />
         </a>
          <a href={projects.live} target='blank' title='live site'>
           <FaExternalLinkAlt className='text-green-500 text-2xl hover:text-gray-400 transition' />
